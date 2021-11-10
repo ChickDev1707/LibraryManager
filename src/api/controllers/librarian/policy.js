1a1d4669bc4597b59729ce57e536b7940afce7bc
ChickDev1707 <thuancoixy234786@gmail.com>
2021-11-10 11:40:39 +0700

    add book category logic

--- src/api/controllers/librarian/policy.js
+++ src/api/controllers/librarian/policy.js
@@ -1,18 +1,41 @@
-const readerPolicyService = require('../../services/librarian/policy.js')
+const policyService = require('../../services/librarian/policy.js')
 
+//reader
 async function getPolicyMainPage(req, res){
-  const readerPolicies = await readerPolicyService.getReaderPolicies()
+  const readerPolicies = await policyService.getReaderPolicies()
   res.render('librarian/policy/main.ejs', {readerPolicies})
 }
 async function updateReaderPolicies(req, res){
   const policiesInput = req.body
   Object.keys(policiesInput).forEach((key, index)=>{policiesInput[key] = Number(policiesInput[key])})
   // parse form field policies to number
-  await readerPolicyService.updateReaderPolicies(policiesInput)
+  await policyService.updateReaderPolicies(policiesInput)
   res.redirect('/librarian/policy')
 }
-function getPolicyBookPage(req, res){
-  res.render('librarian/policy/book.ejs')
+//book category
+async function getPolicyCategoryPage(req, res){
+  const bookCategories = await policyService.getBookCategories()
+  res.render('librarian/policy/book-category.ejs', {bookCategories})
+}
+// add
+async function addNewBookCategory(req, res){
+  const categoryName = req.body.categoryName
+  await policyService.addNewBookCategory(categoryName)
+  res.redirect('/librarian/policy/book-category')
+}
+// edit
+async function editBookCategory(req, res){
+  const category = {
+    id: req.params['id'],
+    name: req.body.categoryName
+  }
+  await policyService.editBookCategory(category)
+  res.redirect('/librarian/policy/book-category')
+}
+async function deleteBookCategory(req, res){
+  const categoryId = req.params['id']
+  await policyService.deleteBookCategory(categoryId)
+  res.redirect('/librarian/policy/book-category')
 }
 
 function getPolicyBorrowBookPage(req, res){
@@ -25,7 +48,11 @@ module.exports ={
   getPolicyMainPage,
   updateReaderPolicies,
 
-  getPolicyBookPage,
+  getPolicyCategoryPage,
+  addNewBookCategory,
+  editBookCategory,
+  deleteBookCategory,
+  
   getPolicyBorrowBookPage,
   getPolicyFinePage
 }
--- src/api/services/librarian/policy.js
+++ src/api/services/librarian/policy.js
@@ -1,13 +1,14 @@
-const Policy = require("../../models/policy")
-
+const Policy = require('../../models/policy')
+const BookCategory = require('../../models/book-category')
+// reader
 async function getReaderPolicies(){
-  const minAge = await Policy.findOne({ten_quy_dinh: "tuoi_toi_thieu"})
-  const maxAge = await Policy.findOne({ten_quy_dinh: "tuoi_toi_da"})
-  const cardExpireLimit = await Policy.findOne({ten_quy_dinh: "thoi_han_the"})
+  const minAge = await Policy.findOne({ten_quy_dinh: 'tuoi_toi_thieu'})
+  const maxAge = await Policy.findOne({ten_quy_dinh: 'tuoi_toi_da'})
+  const cardExpireLimit = await Policy.findOne({ten_quy_dinh: 'thoi_han_the'})
   return {
-    minAge: minAge != null? minAge.gia_tri: "", 
-    maxAge: maxAge != null? maxAge.gia_tri: "",
-    cardExpireLimit: cardExpireLimit != null? cardExpireLimit.gia_tri: ""
+    minAge: minAge != null? minAge.gia_tri: '', 
+    maxAge: maxAge != null? maxAge.gia_tri: '',
+    cardExpireLimit: cardExpireLimit != null? cardExpireLimit.gia_tri: ''
   }
 }
 
@@ -17,33 +18,60 @@ async function updateReaderPolicies(policiesInput){
   await handleUpdateCardExpireLimitAgePolicy(policiesInput.cardExpireLimit)
 }
 async function handleUpdateMinAgePolicy(minAgeInput){
-  let minAgePol = await Policy.findOne({ten_quy_dinh: "tuoi_toi_thieu"})
+  let minAgePol = await Policy.findOne({ten_quy_dinh: 'tuoi_toi_thieu'})
   if(minAgePol == null){
-    minAgePol = new Policy({ten_quy_dinh: "tuoi_toi_thieu", gia_tri: minAgeInput})
+    minAgePol = new Policy({ten_quy_dinh: 'tuoi_toi_thieu', gia_tri: minAgeInput})
   }else{
     minAgePol.gia_tri = minAgeInput
   }
   await minAgePol.save()
 }
 async function handleUpdateMaxAgePolicy(maxAgeInput){
-  let maxAgePol = await Policy.findOne({ten_quy_dinh: "tuoi_toi_da"})
+  let maxAgePol = await Policy.findOne({ten_quy_dinh: 'tuoi_toi_da'})
   if(maxAgePol == null){
-    maxAgePol = new Policy({ten_quy_dinh: "tuoi_toi_da", gia_tri: maxAgeInput})
+    maxAgePol = new Policy({ten_quy_dinh: 'tuoi_toi_da', gia_tri: maxAgeInput})
   }else{
     maxAgePol.gia_tri = maxAgeInput
   }
   await maxAgePol.save()
 }
 async function handleUpdateCardExpireLimitAgePolicy(cardExpireLimitInput){
-  let cardExpireLimit = await Policy.findOne({ten_quy_dinh: "thoi_han_the"})
+  let cardExpireLimit = await Policy.findOne({ten_quy_dinh: 'thoi_han_the'})
   if(cardExpireLimit == null){
-    cardExpireLimit = new Policy({ten_quy_dinh: "thoi_han_the", gia_tri: cardExpireLimitInput})
+    cardExpireLimit = new Policy({ten_quy_dinh: 'thoi_han_the', gia_tri: cardExpireLimitInput})
   }else{
     cardExpireLimit.gia_tri = cardExpireLimitInput
   }
   await cardExpireLimit.save()
 }
+// book category
+async function getBookCategories(){
+  const bookCategories = await BookCategory.find({})
+  return bookCategories
+}
+// add
+async function addNewBookCategory(categoryName){
+  const newCat = new BookCategory({ten_the_loai: categoryName})
+  await newCat.save()
+}
+// edit
+async function editBookCategory(category){
+  let dbCategory = await BookCategory.findById(category.id)
+  dbCategory.ten_the_loai = category.name
+  await dbCategory.save()
+}
+// delete
+async function deleteBookCategory(categoryId){
+  let dbCategory = await BookCategory.findById(categoryId)
+  await dbCategory.remove()
+}
+
 module.exports={
   getReaderPolicies,
-  updateReaderPolicies
+  updateReaderPolicies,
+
+  getBookCategories,
+  addNewBookCategory,
+  editBookCategory,
+  deleteBookCategory
 }
