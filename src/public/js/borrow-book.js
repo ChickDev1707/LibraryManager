21918293a0b83a8c78c80416413f957d7fc4d60c
TranNguyen01 <67365431+TranNguyen01@users.noreply.github.com>
2021-12-09 18:48:16 +0700

    fix manage book

--- src/public/js/borrow-book.js
+++ src/public/js/borrow-book.js
@@ -1,65 +1,57 @@
 
-function deleteBook(id){
+function deleteBook(id) {
     $(`tr#dau_sach_${id}`).remove();
     $(`table#add_table button#add_${id}`).prop("disabled", false);
 }
 
-function submit(){
+function submit() {
     var dau_sach = [];
-    var ma_sach= [];
-    $("#borrow_table tbody td.dau_sach").each(function() {
+    var ma_sach = [];
+    $("#borrow_table tbody td.dau_sach").each(function () {
         dau_sach.push($(this).attr("value"))
     })
 
-    $("#borrow_table tbody td.ma_sach").each(function() {
+    $("#borrow_table tbody td.ma_sach").each(function () {
         ma_sach.push($(this).attr("value"))
     })
 
-    if(dau_sach.length <=0 || ma_sach.length <=0)
-    {  
-        const result = { success: false, message:"Vui lòng chọn sách trước khi tạo phiếu!"   };
+    if (dau_sach.length <= 0 || ma_sach.length <= 0) {
+        const result = { success: false, message: "Vui lòng chọn sách trước khi tạo phiếu!" };
         showToast(result);
-        return ;
+        return;
     }
 
     var ma_doc_gia = $("#reader_id").val();
-    if(ma_doc_gia == "")
-    {
-        const result = { success: false, message:"Vui lòng nhập thông tin độc giả để tạo phiếu!"  };
+    if (ma_doc_gia == "") {
+        const result = { success: false, message: "Vui lòng nhập thông tin độc giả để tạo phiếu!" };
         showToast(result);
-        return ;
+        return;
     }
 
     var ngay_muon = $("#ngay_muon").val();
 
-    if(!checkDate(ngay_muon)){
-        changeToast({
-            type: "error",
-            message: "Ngày mượn không được vượt quá thời gian hiện tại!" 
-        })
-        toast.show();
-        return ;
+    if (!checkDate(ngay_muon)) {
+        const result = { success: false, message: "Ngày mượn không được vượt quá thời gian hiện tại!" }
+        showToast(result)
+        return;
     }
 
     $.ajax({
         type: "POST",
         contentType: "application/json",
         url: "/librarian/borrow",
-        data: JSON.stringify({dau_sach: dau_sach,ma_sach: ma_sach ,ma_doc_gia: ma_doc_gia, ngay_muon:ngay_muon}),
+        data: JSON.stringify({ dau_sach: dau_sach, ma_sach: ma_sach, ma_doc_gia: ma_doc_gia, ngay_muon: ngay_muon }),
         dataType: 'json',
         cache: false,
         success: function (result) {
-            if(result.error){
-                changeToast({
-                    type: "error",
-                    message: result.message 
-                })
-                toast.show();
-                return ;
+            if (result.error) {
+                const messageToast = { success: false, message: result.message };
+                showToast(messageToast)
+                return;
             }
 
             $("table#result_table tbody").empty();
-            result.nSuccess.map((item)=>{
+            result.nSuccess.map((item) => {
                 var row = `
                 <tr>
                     <td scope="col">${item.ten_dau_sach}</td>
@@ -72,7 +64,7 @@ function submit(){
                 deleteBook(item.dau_sach)
             })
 
-            result.nErrors.map((item)=>{
+            result.nErrors.map((item) => {
                 var row = `
                 <tr>
                     <td scope="col">${item.ten_dau_sach}</td>
@@ -88,118 +80,118 @@ function submit(){
             $('#result_modal').modal('show')
         },
         error: function (e) {
-            const result = { success: false, message:"Không thành công!"  };
+            const result = { success: false, message: "Không thành công!" };
             showToast(result);
         }
     });
 }
 
-function checkDate(dateString){
-    var date = new Date(dateString);
-    var toDate = new Date();
-    return date <= toDate
+function checkDate(dateString) {
+    var q = new Date();
+    var m = q.getMonth();
+    var d = q.getDate();
+    var y = q.getFullYear();
+
+    var toDate = new Date(y, m, d)
+    var date = new Date(dateString + " GMT+0700");
+
+    return date <= toDate;
 }
 
 
-function subConfirm(){
+function subConfirm() {
     var ids = []
-    $("input.confirm_input:checked").each(function() {
+    $("input.confirm_input:checked").each(function () {
         ids.push($(this).val())
     })
 
-    if(ids.length == 0)
-    {
-        const result = { success: false, message:"Vui lòng chọn phiếu đăng ký cần xác nhận lấy sách!" };
+    if (ids.length == 0) {
+        const result = { success: false, message: "Vui lòng chọn phiếu đăng ký cần xác nhận lấy sách!" };
         showToast(result);
-        return ;
-    }   
+        return;
+    }
 
     var ngayMuon = $("#ngay_muon").val()
-    if(!checkDate(ngayMuon)){
-        changeToast({
-            type: "error",
-            message: "Ngày lấy không được vượt quá thời gian hiện tại!" 
-        })
-        toast.show();
-        return ;
+    if (!checkDate(ngayMuon)) {
+        const result = { success: false, message: "Ngày lấy không được vượt quá thời gian hiện tại!" };
+        showToast(result);
+        return;
     }
     $.ajax({
-            type: "POST",
-            contentType: "application/json",
-            url: "/librarian/borrow/confirm",
-            data: JSON.stringify({dsMaPhieu: ids, ngayMuon:ngayMuon}),
-            dataType: 'json',
-            cache: false,
-            timeout: 60000,
-            success: function (result) {
-                console.log(result)
-                if(result.nErrors==0 && result.nSuccess==0){
-                    const result = { success: false, message: result.message };
-                    showToast(result);
-                    return ;
-                }
-                
-                result.nSuccess.forEach(id => {
-                    $(`input[value="${id}"].confirm_input`).prop('checked', false);
-                    $(`input[value="${id}"].confirm_input`).prop('disabled', true);
-                    $(`td.status#sts_${id}`).html("Đã nhận sách");
-                    $(`td.date_borow#date_borow_${id}`).html(new Date(ngayMuon).toLocaleDateString('en-GB'));
-
-                    
-                });
-               
-                if(result.nSuccess.length > 0){
-                    const result = { success: true, message: `Xác nhận thành công phiếu có mã:\n${result.nSuccess.join(',\n')}` };
-                    showToast(result);
-                }
-                
-                if(result.nErrors.length > 0){
-                    const result = { success: false, message: `Xác nhận không thành công phiếu có mã:\n${result.nSuccess.join(',\n')}` };
-                    showToast(result);
-                }
-
-               
-               
-                
-            },
-            error: function (e) {
-                const result = { success: false, message: `Xác nhận không thành công!` };
+        type: "POST",
+        contentType: "application/json",
+        url: "/librarian/borrow/confirm",
+        data: JSON.stringify({ dsMaPhieu: ids, ngayMuon: ngayMuon }),
+        dataType: 'json',
+        cache: false,
+        timeout: 60000,
+        success: function (result) {
+            console.log(result)
+            if (result.nErrors == 0 && result.nSuccess == 0) {
+                const result = { success: false, message: result.message };
                 showToast(result);
-                return ;
+                return;
             }
-        });
+
+            result.nSuccess.forEach(id => {
+                $(`input[value="${id}"].confirm_input`).prop('checked', false);
+                $(`input[value="${id}"].confirm_input`).prop('disabled', true);
+                $(`td.status#sts_${id}`).html("Đã nhận sách");
+                $(`td.date_borow#date_borow_${id}`).html(new Date(ngayMuon).toLocaleDateString('en-GB'));
+
+
+            });
+
+            if (result.nSuccess.length > 0) {
+                const result = { success: true, message: `Xác nhận thành công phiếu có mã:\n${result.nSuccess.join(',\n')}` };
+                showToast(result);
+            }
+
+            if (result.nErrors.length > 0) {
+                const result = { success: false, message: `Xác nhận không thành công phiếu có mã:\n${result.nSuccess.join(',\n')}` };
+                showToast(result);
+            }
+
+
+
+
+        },
+        error: function (e) {
+            const result = { success: false, message: `Xác nhận không thành công!` };
+            showToast(result);
+            return;
+        }
+    });
 }
 
-function addBorrowBook(event){
+function addBorrowBook(event) {
     event.preventDefault();
-   
+
     var form = event.target;
     var formData = new FormData(form);
     var ma_sach = formData.get('ma_sach');
     var ma_doc_gia = formData.get('ma_doc_gia');
 
-    if($(`td#ma_quyen_sach_${ma_sach}`).length > 0)
-    {
+    if ($(`td#ma_quyen_sach_${ma_sach}`).length > 0) {
         const result = { success: false, message: "Sách này đã được thêm!" };
         showToast(result);
         return false;
     }
-    else{
+    else {
         $.ajax({
-            method:  "GET",
+            method: "GET",
             url: $(form).attr('action'),
-            data: {ma_sach: ma_sach, ma_doc_gia: ma_doc_gia},      
+            data: { ma_sach: ma_sach, ma_doc_gia: ma_doc_gia },
             timeout: 6000,
             success: function (result) {
-                if(result.success){
+                if (result.success) {
                     var bookBorrow = result.book;
-                    if($(`tr#dau_sach_${bookBorrow._id}`).length > 0)
-                    {
+                    if ($(`tr#dau_sach_${bookBorrow._id}`).length > 0) {
                         const result = { success: false, message: "Đầu sách này đã được thêm!" }
                         showToast(result)
                         return false;
-                    }else{
-                        var content =  
+                    } else {
+                        var content =
                             `<tr id="dau_sach_${bookBorrow._id}">
                                 <td class="align-middle">
                                     <img height="142" width="100" class="rounded" src="${bookBorrow.anh_bia}"/>
@@ -225,7 +217,7 @@ function addBorrowBook(event){
 
                         $("#row_add_button").before(content);
                     }
-                   
+
                 }
                 showToast(result)
             },
@@ -237,24 +229,24 @@ function addBorrowBook(event){
 
         return false;
     }
-    
+
 }
 
 
-function showToast(result){
+function showToast(result) {
     let messageToast = document.getElementById('message-toast');
     messageToast.parentElement.style.zIndex = 9999;
     messageToast.classList.remove('d-none');
     var toast = new bootstrap.Toast(messageToast)
     changeToast({
-            type: result.success?"success":"error",
-            message: result.message 
+        type: result.success ? "success" : "error",
+        message: result.message
     })
     toast.show();
 }
 
-$(document).ready(function(){
+$(document).ready(function () {
     $('#message-toast').on('hidden.bs.toast', function () {
-            $('#message-toast').addClass('d-none')
+        $('#message-toast').addClass('d-none')
     })
 })
