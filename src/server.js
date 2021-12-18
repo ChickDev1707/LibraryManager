2feba19be8a9a6072ab00a21ea47768365405e9f
ChickDev1707 <thuancoixy234786@gmail.com>
2021-12-19 00:26:37 +0700

    fix port bug

--- src/server.js
+++ src/server.js
@@ -15,9 +15,9 @@ const path = require('path')
 const cors = require('cors')
 global.appRoot = path.resolve(__dirname, "..");
 // includes routes
-// const librarianRoute = require('./api/routes/librarian.js')
-// const librarianRoute=require('./api/routes/librarian')
-// const readerRoute = require('./api/routes/reader.js')
+const librarianRoute = require('./api/routes/librarian.js')
+const librarianRoute=require('./api/routes/librarian')
+const readerRoute = require('./api/routes/reader.js')
 const userRoute = require('./api/routes/user.js')
 
 const app = express()
@@ -56,9 +56,9 @@ app.use(passport.initialize())
 app.use(passport.session())
 app.use(cors())
 
-// routes
-// app.use('/librarian', librarianRoute)
-// app.use('/reader', readerRoute)
+routes
+app.use('/librarian', librarianRoute)
+app.use('/reader', readerRoute)
 app.use('/', userRoute(passport))
 
 // database
